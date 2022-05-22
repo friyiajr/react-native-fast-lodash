d3011f4b560e603cd24da6df852f7675c9a41f51
Daniel Friyia <dfriyia@gmail.com>
2022-05-21 22:55:24 -0400

    [DF] chore: fix global declaration

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-// @ts-nocheck
-
 import { NativeModules } from 'react-native';
 
-NativeModules.FastLodash.install()
+NativeModules.FastLodash.install();
+
+declare global {
+  function jsiGcd(values: object): number;
+}
 
 export function gcd(values: object): number {
   return global.jsiGcd(values);
 }
-
-
