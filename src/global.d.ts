7aa04c156c15b56ef20c0aa80936f2424739d88e
Daniel Friyia <dfriyia@gmail.com>
2022-05-23 16:05:09 -0400

    [DF] feat: add max and min functions

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,6 @@
 declare module global {
   function jsiCeil(num: number, precision: number): number;
   function jsiFloor(num: number, precision: number): number;
+  function jsiMax(num: number[]): number;
+  function jsiMin(num: number[]): number;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,17 @@ function floor(num: number, precision: number = 0): number {
   return global.jsiFloor(num, precision);
 }
 
+function max(num: number[]): number {
+  return global.jsiMax(num);
+}
+
+function min(num: number[]): number {
+  return global.jsiMin(num);
+}
+
 export default {
   ceil,
   floor,
+  max,
+  min,
 };
